7b2e7a56631a0ce06278bfd5e619aba4b96edfad
beamhealthtyler <tyler@beamhealthgroup.com>
2022-02-17 15:00:47 -0800

    landing page init

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
             <div className="beam-health-header-text">
               <div>Whether you are a new practice trying to acquire patients or a healthcare enterprise with thousands of clinicians, you need a fail-proof way to see patients remotely.</div>
               <div>You can use Beam’s telehealth functionality for free, right now! Try your own mock telehealth visit below.</div>
-              <button type="button" class="btn btn-secondary">Secondary</button>
+              <button type="button" class="btn btn-secondary">Try Beam Free</button>
             </div>
           </div>
         </div>
@@ -37,11 +37,11 @@ function App() {
               <div>HD, HIPPA Compliant Telehealth</div>
               <div>Scalable Analytics</div>
               <div>Intuitive Scheduling</div>
-              <button type="button" class="btn btn-secondary">Secondary</button>
+              <button type="button" class="btn btn-secondary">Learn More</button>
             </div>
           </div>
           <div className='col-6'>
-            <img className='image' src='https://beam.health/wp-content/uploads/2021/11/Image-124-2.png'></img>
+            <img className='image section2-image' src='https://beam.health/wp-content/uploads/2021/11/Image-124-2.png'></img>
           </div>
         </div>
 
@@ -50,7 +50,7 @@ function App() {
 
         <div className='row section3-background'>
           <div className='col-6'>
-            <div>
+            <div className='mascot-text'>
               <div>How much more can you make with Beam?</div>
               <div>Find out in 2 minutes how much you lost due to your patient collections process</div>
               <button type="button" class="btn btn-secondary">See Your Savings</button>
