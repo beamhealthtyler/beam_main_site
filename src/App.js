027787f6f00dec621d1029d73ef79715d5a78e13
beamhealthtyler <tyler@beamhealthgroup.com>
2022-02-17 15:24:17 -0800

    mobile

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
         </div>
 
         <div className='row'>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-12'>
             <video className='video' autoplay loop muted src="https://beam.health/wp-content/uploads/2021/11/ezgif.com-gif-maker-1.mp4" playsinline></video>
           </div>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-0'>
             <div className="beam-health-header-text">
               <div>Whether you are a new practice trying to acquire patients or a healthcare enterprise with thousands of clinicians, you need a fail-proof way to see patients remotely.</div>
               <div>You can use Beam’s telehealth functionality for free, right now! Try your own mock telehealth visit below.</div>
@@ -27,7 +27,7 @@ function App() {
         </div>
 
         <div className='row section2'>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-12'>
             <div className='section2-text'>
               <div className='section2-header'>Features Providers and Patients Love</div>
               <div className='blue-background'>
@@ -40,30 +40,31 @@ function App() {
               <button type="button" class="btn btn-secondary">Learn More</button>
             </div>
           </div>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-12'>
             <img className='image section2-image' src='https://beam.health/wp-content/uploads/2021/11/Image-124-2.png'></img>
           </div>
         </div>
+      </div>
 
 
         {/* Featured On Section*/}
 
         <div className='row section3-background'>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-12'>
             <div className='mascot-text'>
               <div>How much more can you make with Beam?</div>
               <div>Find out in 2 minutes how much you lost due to your patient collections process</div>
               <button type="button" class="btn btn-secondary">See Your Savings</button>
             </div>
           </div>
-          <div className='col-6'>
+          <div className='col-md-6 col-sm-12'>
             <img className='image' src='https://beam.health/wp-content/uploads/2021/11/mascot_03.png'></img>
           </div>
         </div>
 
         <div className='row'>
           <div className='col'>
-            <div>Craft and Measure the Patient Experience</div>
+            <div class='craft-and-measure'>Craft and Measure the Patient Experience</div>
           </div>
         </div>
 
@@ -75,7 +76,6 @@ function App() {
 
 
 
-      </div>
     </div>
   );
 }
