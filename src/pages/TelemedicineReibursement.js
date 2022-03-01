ec443f57980d7b3f660ccd642bbb4ede3fabfaab
Tyler Bray <tylerbray@tylers-air.lan>
2022-03-01 10:52:27 -0800

    telemedicine

--- src/pages/TelemedicineReibursement.js
+++ src/pages/TelemedicineReibursement.js
@@ -1,5 +1,64 @@
+import Style from './Styles/Telemedicine.module.css'
+
 const TelemedicineReibursement = () => {
-  return <h1>Telemedicine Reibursement</h1>;
+  return (
+    <>
+      <div className='container'>
+        <div className={"row " + Style.section}>
+          <div className="col-md-6">
+            <img className='image1' src="https://beam.health/wp-content/uploads/2021/12/image-4.png"></img>
+          </div>
+          <div className="col-md-6">
+            <h1 className='title'>Know where your state stands on telemedicine.</h1>
+            <p>Recent advances in reimbursement policies have paved the way for you to build a prospering telehealth line of business.</p>
+            <p>Fill out the form below to be contacted by one of our Integration Specialists</p>
+          </div>
+        </div>
+
+        <div className={"row " + Style.section}>
+          <div className="col-md-6">
+            <p>Telemedicine is an exciting and constantly evolving technology that has been embraced by healthcare professionals all over the world. As its use continues to expand, insurance providers and states across the country are seeing the utility in telemedicine and are encompassing it in their plans.</p>
+            <p>In fact, not only have regulatory bodies embraced the use of telemedicine for providers in the states that they are licensed in, but laws are now allowing for virtual care across state boundaries. 12 state boards allow special telemedicine certifications to allow providers to practice medicine across state lines via telemedicine (AL, LA, NV, NJ, NM, OH, OK, OR, PA, TN, TX).</p>
+            <p>Below is a guide for what you need to know when seeking reimbursement for telemedicine. Please visit our comprehensive, state by state policy guide to see your state’s telemedicine insurance laws.</p>
+            <button>Request a Demo</button>
+          </div>
+          <div className="col-md-6">
+            <img className='image1' src="https://beam.health/wp-content/uploads/2021/12/image-4-1.png"></img>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={Style.backgroundImage}>
+        <div className='container'>
+          <div className={'row ' + Style.sectionPadding}>
+            <div className='col'>
+              <h1 style={{textAlign: 'center'}}>What You Need to Know</h1>
+              <p>Private parity laws</p>
+              <p>Medicare</p>
+              <p>Medicaid Coverage for Telemedicine</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className={'row ' + Style.sectionPadding}>
+          <div className='col'>
+            <h1 style={{textAlign: 'center'}}>Types of Telemedicine</h1>
+            <p style={{textAlign: 'center'}}>There are three types of telemedicine that states recognize for insurance purposes: Live Video, Store-and-Forward, and Remote Patient Monitoring. Each of these services helps different providers in different ways, however, not all types of telemedicine are created equally. You’ll find that on a state by state basis, Live Video telemedicine is considered the gold standard for reimbursement. Live Video is much more prevalent, widely used, and covered by Medicaid and parity laws than both Store-and-Forward and Remote Patient Monitoring. Beam supports Live Video</p>
+            <p>Live Video</p>
+            <p>Store-and-Forward</p>
+            <p>Remote Patient Monitoring</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Carasoul Component */}
+
+
+    </>
+  )
 };
 
 export default TelemedicineReibursement;
