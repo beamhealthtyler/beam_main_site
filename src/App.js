7d24d249013fe42f11b9433972efe56e39f9a89e
Tyler Bray <tylerbray@tylers-air.lan>
2022-03-09 23:11:32 -0800

    hasrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
@@ -25,7 +25,7 @@ import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
              <Route path="/" element={<Layout />}>
                <Route index element={<Home />} />
@@ -51,6 +51,6 @@ export default function App() {
                <Route path="*" element={<NoPage />} />
              </Route>
            </Routes>
-         </BrowserRouter>
+         </HashRouter>
   );
 }
