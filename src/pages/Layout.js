56a745281f0fb1fe1ec405ca73d10f38cae4fa83
Tyler Bray <tylerbray@tylers-air.lan>
2022-03-10 19:25:45 -0800

    navigation

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/home">Beam Health</Navbar.Brand>
+          <Navbar.Brand onClick={() => navigate("/home")}>Beam Health</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -15,29 +15,29 @@ const Layout = () => {
 
              <Nav.Link onClick={() => navigate("/about")}>About Us</Nav.Link>
 
-              <Nav.Link href="/demo">Book a Demo</Nav.Link>
+              <Nav.Link onClick={() => navigate("/demo")}>Book a Demo</Nav.Link>
               <NavDropdown title="Solutions" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/solutions">Solutions</NavDropdown.Item>
-                <NavDropdown.Item href="/mentalhealth">Mental Health</NavDropdown.Item>
-                <NavDropdown.Item href="/patientengagement">Patient Engagement</NavDropdown.Item>
-                <NavDropdown.Item href="/telehealth-landing-page">Telehealth</NavDropdown.Item>
-                <NavDropdown.Item href="/online-payments">Online Payments</NavDropdown.Item>
-                <NavDropdown.Item href="/patient-intake">Patient Intake</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/solutions")}>Solutions</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/mentalhealth")}>Mental Health</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/patientengagement")}>Patient Engagement</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/telehealth-landing-page")}>Telehealth</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/online-payments")}>Online Payments</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/patient-intake")}>Patient Intake</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Who We Help" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/enterprise-page">Enterprise Page</NavDropdown.Item>
-                <NavDropdown.Item href="/for-clinics">For Clinics</NavDropdown.Item>
-                <NavDropdown.Item href="/affiliate-partnerships-reseller-program">Affiliate Partnerships</NavDropdown.Item>
-                <NavDropdown.Item href="/patients">Patients</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/enterprise-page")}>Enterprise Page</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/for-clinics")}>For Clinics</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/affiliate-partnerships-reseller-program")}>Affiliate Partnerships</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/patients")}>Patients</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Resources" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/blogs">Blogs</NavDropdown.Item>
-                <NavDropdown.Item href="/ehr-integrations">EHR Integrations</NavDropdown.Item>
-                <NavDropdown.Item href="/case-studies">Case Studies</NavDropdown.Item>
-                <NavDropdown.Item href="/state-telemedicine-policies">State Telemedicine Policies</NavDropdown.Item>
-                <NavDropdown.Item href="/telemedicine-reibursement">Telemedicine Reibursement</NavDropdown.Item>
-                <NavDropdown.Item href="/faq">FAQ</NavDropdown.Item>
-                <NavDropdown.Item href="/beam-vs-others">Beam Vs Others</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/blogs")}>Blogs</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/ehr-integrations")}>EHR Integrations</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/case-studies")}>Case Studies</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/state-telemedicine-policies")}>State Telemedicine Policies</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/telemedicine-reibursement")}>Telemedicine Reibursement</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/faq")}>FAQ</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => navigate("/beam-vs-others")}>Beam Vs Others</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
